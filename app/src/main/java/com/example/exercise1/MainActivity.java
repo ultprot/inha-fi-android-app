649ebd97384b6baf2439144ddebe647541eb4b97
choieujin <gch04750@gmail.com>
2018-08-15 22:28:59 +0900

    login with kakao

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -3,27 +3,118 @@ package com.example.exercise1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private SessionCallback callback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView executeBtn = findViewById(R.id.imageView4);
-        executeBtn.setOnClickListener(new OnClickListener(){
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        Session.getCurrentSession().checkAndImplicitOpen();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+    //for redirect page after login success
+    protected void redirectSignupActivity() {
+        Log.e("redirect","in redirect Singup Activity");
+
+        Intent executeIntent = new Intent(getApplicationContext(),ExecuteActivity.class);
+        startActivity(executeIntent);
+    }
+
+    //kakao Login Callback
+    private class SessionCallback implements ISessionCallback {
+
+        // 로그인에 성공한 상태
+        @Override
+        public void onSessionOpened() {
+            Log.e("session opned","open success");
+            redirectSignupActivity();
+        }
+
+        // 로그인에 실패한 상태
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
+            }
+    }
+
+
+    // kakao 사용자 정보 요청
+    public void requestMe() {
+        // 사용자정보 요청 결과에 대한 Callback
+        UserManagement.getInstance().requestMe(new MeResponseCallback() {
+            // 세션 오픈 실패. 세션이 삭제된 경우,
             @Override
-            public void onClick(View view) {
-                Intent executeIntent = new Intent(getApplicationContext(),ExecuteActivity.class);
-                startActivity(executeIntent);
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.e("SessionCallback :: ", "onSessionClosed : " + errorResult.getErrorMessage());
+            }
+
+            // 회원이 아닌 경우,
+            @Override
+            public void onNotSignedUp() {
+                Log.e("SessionCallback :: ", "onNotSignedUp");
+            }
+
+            // 사용자정보 요청에 성공한 경우,
+            @Override
+            public void onSuccess(UserProfile userProfile) {
+                Log.e("SessionCallback :: ", "onSuccess");
+
+                String nickname = userProfile.getNickname();
+                String email = userProfile.getEmail();
+                String profileImagePath = userProfile.getProfileImagePath();
+                String thumnailPath = userProfile.getThumbnailImagePath();
+                String UUID = userProfile.getUUID();
+                long id = userProfile.getId();
+                Log.e("Profile : ", nickname + "");
+                Log.e("Profile : ", email + "");
+                Log.e("Profile : ", profileImagePath + "");
+                Log.e("Profile : ", thumnailPath + "");
+                Log.e("Profile : ", UUID + "");
+                Log.e("Profile : ", id + "");
+
+            }
+
+            // 사용자 정보 요청 실패
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                Log.e("SessionCallback :: ", "onFailure : " + errorResult.getErrorMessage());
             }
         });
-
     }
 }
