f888b8cec44da99fe47ea1ed84a76e9bafb6fa6b
choieujin <gch04750@gmail.com>
2018-08-21 19:34:00 +0900

    문자,전화,구글아이디로그인,로그아웃

--- app/src/main/java/com/example/exercise1/ExecuteActivity.java
+++ app/src/main/java/com/example/exercise1/ExecuteActivity.java
@@ -1,9 +1,13 @@
 package com.example.exercise1;
 
-import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,25 +15,28 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.LogoutResponseCallback;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,15 +48,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class ExecuteActivity extends AppCompatActivity {
+
+import android.support.v7.app.AlertDialog;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import android.Manifest;
+import android.telephony.SmsManager;
+
+
+public class ExecuteActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
 
     Button button;
@@ -71,12 +85,24 @@ public class ExecuteActivity extends AppCompatActivity {
     private TextToSpeech myTTS;
     private SpeechRecognizer mySpeechRecognizer;
 
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+
+    private Button mMessage;
+    private Button mCall;
+    private final int PERMISSIONS_REQUEST_RESULT = 1;
+
     TextView Path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_execute);
+
+        //문자전송
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_PHONE_STATE}, PERMISSIONS_REQUEST_RESULT);
+        }
 
         SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
         String userId = pref.getString("userId","");
@@ -93,6 +119,21 @@ public class ExecuteActivity extends AppCompatActivity {
         initializeTextToSpeech();
         initializeSpeechRecognizer();
 
+        // GoogleSignInOptions 생성
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder
+                (GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this )
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // 로그인 작업의 onCreate 메소드에서 FirebaseAuth 개체의 공유 인스턴스를 가져옵니다.
+        mAuth = FirebaseAuth.getInstance();
+
         Button logoutBtn = (Button) findViewById(R.id.logoutBtn);
         logoutBtn.setOnClickListener(new OnClickListener() {
             @Override
@@ -100,12 +141,45 @@ public class ExecuteActivity extends AppCompatActivity {
                 // 서버에 유저정보 삭제 & 앱 file에 저장된 정보 삭제
                 new SendPostDelete().execute("http://14.63.161.4:26533/deleteuser");
 
-                //logout 수행
-                onClickLogout();
+                SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+
+                String loginWay = pref.getString("login","");
+                if(loginWay == "gogle"){
+                    Log.v("알림", "구글 LOGOUT");
+                    AlertDialog.Builder alt_bld = new AlertDialog.Builder(v.getContext());
+                    alt_bld.setMessage("로그아웃 하시겠습니까?").setCancelable(false)
+                            .setPositiveButton("네",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            // 네 클릭
+                                            // 로그아웃 함수 call
+                                            signOut();
+                                        }
+                                    }).setNegativeButton("아니오",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // 아니오 클릭. dialog 닫기.
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert = alt_bld.create();
+                    // 대화창 클릭시 뒷 배경 어두워지는 것 막기
+                    alert.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                    // 대화창 제목 설정
+                    alert.setTitle("로그아웃");
+                    // 대화창 아이콘 설정
+                    //alert.setIcon(R.drawable.check_dialog_64);
+                    // 대화창 배경 색 설정
+                    alert.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(255, 62, 79, 92)));
+                    alert.show();
+                }else{
+                    //kakaologout 수행
+                    Log.d("알림","카카오 logout");
+                    onClickLogout();
+                }
+
             }
         });
-
-
 
         Button startBtn = (Button) findViewById(R.id.startButton);
         startBtn.setOnClickListener(new OnClickListener() {
@@ -129,6 +203,28 @@ public class ExecuteActivity extends AppCompatActivity {
             }
         });
         //end speech recognition
+
+
+        mMessage = (Button) findViewById(R.id.smsBtn);
+        mCall = (Button) findViewById(R.id.callBtn);
+
+        mMessage.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+                String getGard = pref.getString("gard","");
+                Messenger messenger = new Messenger(getApplicationContext());
+                messenger.sendMessage(getGard, "위급상황입니다");
+            }
+        });
+        mCall.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent("android.intent.action.CALL", Uri.parse("tel:01030159163")));
+            }
+        });
+
+
     }
 
     //벡버튼 두번눌리면 종료
@@ -143,7 +239,7 @@ public class ExecuteActivity extends AppCompatActivity {
         first_time = System.currentTimeMillis();
     }
 
-    public class PostTask extends AsyncTask<String, String, String> {
+        public class PostTask extends AsyncTask<String, String, String> {
         @Override
         protected String doInBackground(String... urls) {
             try {
@@ -588,6 +684,69 @@ public class ExecuteActivity extends AppCompatActivity {
 
                 Log.d("저장", "삭제결과 : " + result);
             }
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.v("알림", "onConnectionFailed");
+    }
+
+    // 로그아웃
+    public void signOut() {
+        mGoogleApiClient.connect();
+
+        mGoogleApiClient.registerConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(@Nullable Bundle bundle) {
+                mAuth.signOut();
+
+                if (mGoogleApiClient.isConnected()) {
+                    Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(@NonNull Status status) {
+                            if (status.isSuccess()) {
+                                SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = pref.edit();
+                                editor.clear();
+                                editor.commit();
+                                Log.d("저장","모든 file 기록 삭제");
+                                Log.v("알림", "로그아웃 성공");
+                                setResult(1);
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                setResult(0);
+                            }
+                            finish();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+                Log.v("알림", "Google API Client Connection Suspended");
+                setResult(-1);
+                finish();
+            }
+        });
+    }
+
+    //sms 전송 , 응급 전화
+    class Messenger {
+        private Context mContext;
+        public Messenger(Context mContext) {
+            this.mContext = mContext;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.DONUT)
+        public void sendMessage(String phoneNum, String message) {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNum, null, message, null, null);
+            Toast.makeText(mContext, "Message transmission is completed.",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
